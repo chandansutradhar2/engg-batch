9b5e8cd801be6a2906af698517ba51ec5bd3a59b
chandan <chandansutradhar2@gmail.com>
2022-11-15 16:17:08 +0530

    day 1

---
+++ demo.js
@@ -0,0 +1,16 @@
+function print(){
+console.log("hello world");
+let demoObj={
+"key1":"value1",
+start:()=>{
+console.log('function start');
+},
+
+stop:()=>{
+}
+}
+
+console.log(demoObj);
+}
+
+print();
